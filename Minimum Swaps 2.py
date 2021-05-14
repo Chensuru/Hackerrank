7fea55a451eb1b719efd4c648d1e051133943bb4
Chensuru <chensuru04351913@gmail.com>
2021-05-14 23:09:51 +0800

    Create Minimum Swaps 2.py

---
+++ Minimum Swaps 2.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    i=0
+    cnt=0
+    for i in range(len(arr)):
+        while arr[i] != i+1:
+            now_num = arr[i]
+            ori_num = arr[now_num-1]
+            arr[i], arr[now_num-1]= ori_num, now_num
+            cnt+=1
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    res = minimumSwaps(arr)
+    fptr.write(str(res) + '\n')
+    fptr.close()
